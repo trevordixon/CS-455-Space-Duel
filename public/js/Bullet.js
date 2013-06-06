db4f345e7398824b8c8af73f273f423e5202374c
Ian Cropper <itcropper@gmail.com>
2013-06-05 21:05:52 -0600

    Bullet Class Created

---
+++ public/js/Bullet.js
@@ -0,0 +1,35 @@
+
+var THREE = require('three');
+
+function Bullet(player, scene){
+
+	this.team = player;
+	this.scene = scene;
+
+	this.bulletShape = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshLambertMaterial({
+		color: this.team == 'red' ? "ff0000" : "#0000ff"
+	}));
+
+	this.bulletShape.overdraw = true;
+	this.scene.add(this.bulletShape);
+
+	console.log("Hey");
+
+}
+
+Bullet.prototype = {
+	addToScene : function(){
+		this.scene.add(this.bulletShape);
+	},
+	setPosition : function(vector){
+		this.position = vector;
+	},
+	setVelocity : function(vel){
+		this.velocity = vel;
+	},
+	setLife : function(alive){
+		this.alive = alive;
+	}
+}
+
+module.exports = Bullet; 
