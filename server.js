89d26dbc3e716651291688c79b428cafa171b35d
itcropper <itcropper@gmail.com>
2013-06-14 13:33:27 -0600

    initialized peer to peer stuff almost there

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ app.get('/js/bundle.js', browserify(bundleModules, {
 	debug: false
 }));
 
+app.get('/getPeerCode', function(req, res){
+
+	var code = req["_parsedUrl"]["query"].split('=')[1];
+	console.log(code);
+	var resp = JSON.stringify(peerCode(code));
+	//console.log(resp);
+	res.write(resp);
+	res.end();
+})
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(8030);
@@ -23,3 +33,19 @@ console.log('Listening on port 8030');
 
 var PeerServer = require('peer').PeerServer;
 var pserver = new PeerServer({ port: 9000 });
+
+
+var playerOne = true;
+
+function peerCode(code){
+	var response = {};
+	if (playerOne){
+		response["player"] = 1;
+		playerOne = false;
+	}else{
+		response["player"] = 2;
+		playerOne = true;
+	}
+	response["code"] = code;
+	return response;
+}
