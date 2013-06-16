6cbbd0a6751260c9e26e5f1d5de8e0866eb1bf43
Trevor Dixon <trevordixon@gmail.com>
2013-06-15 20:38:57 -0600

    Only obey gamepad if window has focus. Lets us test in two different windows.

--- src/game/gamepadManager.js
+++ src/game/gamepadManager.js
@@ -1,9 +1,19 @@
+var gamepad;
 module.exports = {
 	gamepad: navigator.webkitGetGamepads()[0]
 };
 
 setInterval(function() {
-	require('./checkForGamepad.js')(function(gamepad) {
-		module.exports.gamepad = gamepad;
+	require('./checkForGamepad.js')(function(_gamepad) {
+		gamepad = _gamepad;
+		module.exports.gamepad = _gamepad;
 	});
 }, 50);
+
+window.addEventListener('focus', function() {
+	module.exports.gamepad = gamepad;
+});
+
+window.addEventListener('blur', function() {
+	module.exports.gamepad = undefined;
+});
