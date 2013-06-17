fb720d657f921c6e2ac7d7d70d946515055029a8
Trevor Dixon <trevordixon@gmail.com>
2013-06-17 10:48:25 -0600

    Put check for gamepad back.

--- src/game/checkForGamepad.js
+++ src/game/checkForGamepad.js
@@ -1,9 +1,8 @@
-module.exports = function(cb) {
+module.exports = function() {
 	var gamepads = navigator.webkitGetGamepads();
 	for (var i = 0; i < gamepads.length; i++) {
 		if (gamepads[i] && gamepads[i].buttons[0]) {
-			cb(gamepads[i]);
-			break;
+			return i;
 		}
 	}
 };
--- src/game/gamepad.js
+++ src/game/gamepad.js
@@ -1,10 +1,17 @@
-var disabled = false;
+var checkForGamepad = require('./checkForGamepad.js');
+
+var i = 0, disabled = false;
 module.exports = {
 	get: function() {
 		if (disabled) return;
-		return navigator.webkitGetGamepads()[0];
+		return navigator.webkitGetGamepads()[i];
 	}
 };
+
+setInterval(function() {
+	var _i = checkForGamepad();
+	if (_i !== undefined) i = _i;
+}, 100);
 
 window.addEventListener('focus', function() {
 	disabled = false;
